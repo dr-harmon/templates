bf49ee66e07f8e3062cf4f0d992a18afe6d0442b
Trevor Harmon <tharmon@laspositascollege.edu>
2024-01-03 04:04:22 -0800

    Add solution

--- src/matrix.h
+++ src/matrix.h
@@ -4,22 +4,47 @@
 
 template <typename T, int ROWS, int COLS>
 class Matrix {
+    T matrix[ROWS][COLS];
 public:
 
     Matrix() : Matrix({}) {}
 
+    Matrix(T value) {
+        for (int row = 0; row < ROWS; row++) {
+            for (int col = 0; col < COLS; col++) {
+                matrix[row][col] = value;
+            }
+        }
+    }
+
     Matrix operator+(const Matrix &rhs) const {
         Matrix sum;
-        // TODO
+        for (int row = 0; row < ROWS; row++) {
+            for (int col = 0; col < COLS; col++) {
+                sum.matrix[row][col] = matrix[row][col] + rhs.matrix[row][col];
+            }
+        }
         return sum;
     }
 
     bool operator==(const Matrix<T, ROWS, COLS>& rhs) const {
-        // TODO
+        for (int row = 0; row < ROWS; row++) {
+            for (int col = 0; col < COLS; col++) {
+                if (matrix[row][col] != rhs.matrix[row][col]) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     friend std::ostream &operator<<(std::ostream &strm, const Matrix<T, ROWS, COLS> &obj) {
-        // TODO
+        for (int row = 0; row < ROWS; row++) {
+            for (int col = 0; col < COLS; col++) {
+                strm << obj.matrix[row][col] << " ";
+            }
+            strm << std::endl;
+        }
         return strm;
     }
 };
--- test/matrix_test.cpp
+++ test/matrix_test.cpp
@@ -2,12 +2,24 @@
 
 #include "matrix.h"
 
+typedef Matrix<int, 3, 3> Matrix3;
+
 TEST_CASE("Matrices can be added together")
 {
-	// TODO
+	Matrix3 m1(2);
+	Matrix3 m2(3);
+	Matrix3 sum;
+	sum = m1 + m2;
+	Matrix3 expected(5);
+	REQUIRE(sum == expected);
 }
 
 TEST_CASE("Matrices can be compared for equality")
 {
-	// TODO
+	Matrix3 m1(2);
+	Matrix3 m2(3);
+	Matrix3 m3(3);
+	REQUIRE(m1 == m1);
+	REQUIRE_FALSE(m1 == m2);
+	REQUIRE(m2 == m3);
 }
